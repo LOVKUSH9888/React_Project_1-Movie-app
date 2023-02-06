120d5eb26924816e0ae169475fe3a0628ad9e592
LOVKUSH <lovkushyadav9888@gmail.com>
2023-02-06 12:57:36 +0530

    All the comments added in Code

--- src/App.js
+++ src/App.js
@@ -3,50 +3,59 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-//Here is your key: 5b0f3c9f
+// API key for OMDb API
+const API_KEY = "5b0f3c9f";
 
-const API_URL = "http://www.omdbapi.com?apikey=5b0f3c9f";
+// URL for fetching movie data
+const API_URL = `http://www.omdbapi.com?apikey=${API_KEY}`;
 
+// Main component
 const App = () => {
+  // State for search term
   const [searchTerm, setSearchTerm] = useState("");
+
+  // State for movie data
   const [movies, setMovies] = useState([]);
 
+  // useEffect hook to perform an initial search for "Batman"
   useEffect(() => {
     searchMovies("Batman");
   }, []);
 
+  // Function to search for movies by title
   const searchMovies = async (title) => {
+    // Fetch movie data from the API
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
+    // Set the movie data in state
     setMovies(data.Search);
   };
 
   return (
     <div className="app">
-      <h1>Love-movies</h1>
+      {/* Page header */}
+      <h1>Love-Movies</h1>
 
+      {/* Search bar */}
       <div className="search">
-
+        {/* Input field for search term */}
         <input
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search for movies"
         />
 
-
+        {/* Search icon to trigger search */}
         <img
           src={SearchIcon}
           alt="search"
           onClick={() => searchMovies(searchTerm)}
         />
-
-
-
       </div>
 
+      {/* Display movie data */}
       {movies?.length > 0 ? (
-
         <div className="container">
           {movies.map((movie) => (
             <MovieCard movie={movie} />
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,23 +1,30 @@
-
 import React from 'react';
 
+// A functional component that renders a movie card
 const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+  // Using destructuring assignment to extract the properties from the movie object
   return (
+    // Wrapping the content in a div with className "movie" and using imdbID as the key
     <div className="movie" key={imdbID}>
       <div>
+        {/* Displaying the year of the movie */}
         <p>{Year}</p>
       </div>
 
       <div>
+        {/* Displaying the poster image of the movie, using a placeholder image if Poster is "N/A" */}
         <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
       </div>
 
       <div>
+        {/* Displaying the type of the movie */}
         <span>{Type}</span>
+        {/* Displaying the title of the movie */}
         <h3>{Title}</h3>
       </div>
     </div>
   );
 }
 
+// Exporting the component for use in other parts of the application
 export default MovieCard;
